560e55af366de8ec6001583f8ae66d57abe8b55b
Yarusch <impulsimp13@gmail.com>
2023-10-26 16:43:36 +0200

    done

---
+++ js/math.js
@@ -0,0 +1,12 @@
+let  value1= parseFloat(prompt("Enter your first value"));
+
+let  value2= parseFloat(prompt("Enter your second cvalue"));
+
+sum = value1 + value2
+min = value1 - value2
+mul = value1 * value2
+dev = value1 / value2
+
+result = sum + "\n" + min + "\n" + mul + "\n" + dev;
+
+alert(result)
